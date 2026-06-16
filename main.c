7f3d6e423c3af01c0b7d19578bad1b670bb69fc3
Ace120C <vegetotownley@proton.me>
2026-06-16 02:54:50 +0100

    starting to create the main logic

--- main.c
+++ main.c
@@ -1,21 +1,35 @@
+#include <asm-generic/errno-base.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+
 
 int main()
 {
-  chdir("/usr/");
-  pid_t pid = fork();
-  if (pid == -1) {
-    perror("fork failed: not enough memory");
-  }
-  char *args[] = {"ls", NULL};
-  if (pid == 0) {
-    execvp("ls", args);
-    perror("ececvp failed");
+  printf("Checking /usr/ports....\n");
+  chdir("/usr");
+  char targetDir[1024];
+  getcwd(targetDir, sizeof(targetDir));
+
+  if (strcmp(targetDir, "/usr") == 0) {
+    if (mkdir("/usr/ports", 0777) == -1) {
+      if (errno == EEXIST) {
+        printf("Ports Already exist!");
+      }  else if (errno == EACCES) {
+        perror("Portie");
+        printf("try running this with sudo/doas!\n");
+        exit(1);
+      }
+    }
   } else {
-    waitpid(pid, NULL, 0);
+    perror("Portie");
+    exit(1);
   }
+  printf("Sucess!\n");
   return 0;
 }
